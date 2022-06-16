061c4e6f9551e5a6f5a1b5d3f567b62c7456e5a6
Aw <walia_77@outlook.com>
2022-06-16 18:22:20 -0400

    Added Create Snake function

--- main.py
+++ main.py
@@ -6,4 +6,18 @@ screen1.bgcolor("black")
 screen1.title("Snake Game")
 
 
+#create 3 boxes long snake
+def createSnake():
+    x_coordinate = 0
+    y_coordinate = 0
+    for _ in range(0,3):
+        snake1 = []
+        turtle1 = Turtle()
+        turtle1.shape("square")
+        turtle1.color("white")
+        turtle1.goto(x_coordinate,y_coordinate)
+        snake1.append(turtle1)
+        x_coordinate = x_coordinate - 20
+
+createSnake()
 screen1.exitonclick()
